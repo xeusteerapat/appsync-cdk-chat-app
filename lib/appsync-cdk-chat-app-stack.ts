5bab155fb3372204bac50da0ebeaf52fc4e0ebdd
xeusteerapat <xeus085@gmail.com>
2021-08-18 10:00:50 +0700

    :sparkles: [EDIT] update dynamodb resources and permission
    [EDIT] update dynamodb resources and permission

--- lib/appsync-cdk-chat-app-stack.ts
+++ lib/appsync-cdk-chat-app-stack.ts
@@ -57,5 +57,46 @@ export class AppsyncCdkChatAppStack extends cdk.Stack {
     });
 
     // Create DynamoDB resources
+    const messageTable = new Table(this, 'ChatMessageTable', {
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      },
+    });
+
+    const roomTable = new Table(this, 'ChatRoomTable', {
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      },
+    });
+
+    // enable query message by roomId by adding Global Secondary Index
+    messageTable.addGlobalSecondaryIndex({
+      indexName: 'message-by-room-id',
+      partitionKey: {
+        name: 'roomId',
+        type: AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'createdAt',
+        type: AttributeType.STRING,
+      },
+    });
+
+    // Give permission to DynamoDB to allow querying on GSI using IAM
+    const messageTableServiceRole = new Role(this, 'MessageTableServiceRole', {
+      assumedBy: new ServicePrincipal('dynamodb.amazonaws.com'),
+    });
+
+    messageTableServiceRole.addToPolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        resources: [`${messageTable.tableArn}/index/message-by-room-id`],
+        actions: ['dynamodb:Query'],
+      })
+    );
   }
 }
